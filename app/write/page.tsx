cf06bde137d968f5f322f2dd039446aeaf649332
jslee <easysun93@gmail.com>
2024-02-26 00:07:32 +0900

    글쓰기 페이지 css 변경

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -3,13 +3,17 @@ import Link from "next/link";
 export default function Write() {
   return (
     <div className="container">
-      <h4>글작성</h4>
-      <form action="/api/post/new" method="POST">
-        <input name="title" placeholder="제목" />
-        <input name="content" placeholder="내용" />
-        <button type="submit">등록</button>
-        <button><Link href="/list">취소</Link></button>
-      </form>
+      {/* <h4>글작성</h4> */}
+      <div className='content-box'>
+        <form action="/api/post/new" method="POST">
+          <input name="title" placeholder="제목" className="content-title content-title-write" />
+          <input name="content" placeholder="내용" className='content-text content-text-write' />
+          <div className="below-btn">
+            <button type="submit" className="register-btn">등록</button>
+            <button><Link href="/list" className="cancel-btn">취소</Link></button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
