2409bed1237fd6a672a65b1f28fc4fc43277fc59
jslee <easysun93@gmail.com>
2024-02-28 00:14:36 +0900

    회원가입 페이지 수정

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ export default async function Home() {
   return (
     <div className="container list-bg">
       <h4></h4>
-      <hr />
     </div>
   );
 };
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -1,16 +1,52 @@
+import Link from 'next/link';
+
 export default async function SignUp() {
   return (
-    <div className="container">
-      <h4>회원가입</h4>
+    <div className="container signup">
+      <h4 className="signup-title">회원가입</h4>
       <form action="/api/auth/signup" method="POST">
-        <label htmlFor="email">이메일: </label>
-        <input id="email" name="email" type="text" placeholder="아이디" />
-        <label htmlFor="password">비밀번호: </label>
-        <input id="password" name="password" type="password" placeholder="비밀번호" />
-        <label htmlFor="name">이름: </label>
-        <input id="name" name="name" type="text" placeholder="이름" />
+        <div className="signup-input">
+          <label
+            className="signup-input-label"
+            htmlFor="email"
+          >이메일: </label>
+          <input
+            className="signup-input-box"
+            id="email"
+            name="email"
+            type="text"
+            placeholder="이메일" />
+        </div>
+        <div className="signup-input">
+          <label
+            className="signup-input-label"
+            htmlFor="password"
+          >비밀번호: </label>
+          <input
+            className="signup-input-box"
+            id="password"
+            name="password"
+            type="password"
+            placeholder="비밀번호"
+          />
+        </div>
+        <div className="signup-input">
+          <label
+            className="signup-input-label"
+            htmlFor="name">이름: </label>
+          <input
+            className="signup-input-box"
+            id="name"
+            name="name"
+            type="text"
+            placeholder="이름"
+          />
+        </div>
 
-        <button type="submit">가입하기</button>
+        <div className="below-btn">
+          <button type="submit" className="register-btn">가입하기</button>
+          <button><Link href="/list" className="cancel-btn">취소</Link></button>
+        </div>
       </form>
     </div>
   );
