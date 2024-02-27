8d1f8d4eeb20ce9b680bf22d6398cf93bfbd5d41
jslee <jslee1@mobidays.com>
2024-02-27 18:23:42 +0900

    타입 수정

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 
-export default function Comment(props: { _parentId: string }) {
+export default function Comment(props: { _parentId: string | undefined }) {
   const [comment, setComment] = useState("");
   const [commentList, setCommentList] = useState<any[]>([]);
 
--- pages/api/list.ts
+++ pages/api/list.ts
@@ -2,7 +2,12 @@ import { connectDB } from "@/util/database";
 
 export default async function handler(request: any, response: any) {
   const db = (await connectDB).db("myapp");
-  const result = await db.collection("board").find().toArray();
+  const result = await db
+    .collection("board")
+    .find()
+    .sort({ created_time: -1 }) // 날짜 내림차순인데 흠....
+    .toArray();
+  
 
   if (request.method === "GET") {
     return response.status(200).json(result);
