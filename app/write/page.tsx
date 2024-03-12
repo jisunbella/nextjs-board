56ec84d0273c78abbba9f7ea2823e1b322c2a2f5
jslee <jslee1@mobidays.com>
2024-03-12 16:45:42 +0900

    파일첨부 및 이미지 미리보기 (aws s3)

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -1,8 +1,10 @@
 "use client"
 
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Write() {
+  const [imageUrl, setImageUrl] = useState('')
   return (
     <div className="container">
       {/* <h4>글작성</h4> */}
@@ -21,26 +23,27 @@ export default function Write() {
             accept="image/*"
             onChange={async (e) => {
               if (e.target.files) {
-                const file = e.target.files[0]
-                const fileName = encodeURIComponent(file.name) // 한글 파일 명 깨지는거 대비
-                let res: any = await fetch("/api/post/image?file=" + fileName)
-                res = await res.json()
-                console.log("res", res)
-                const formData = new FormData() // form 태그랑 똑같이 동작함
+                const file = e.target.files[0];
+                const fileName = encodeURIComponent(file.name); // 한글 파일명 깨지는거 대비
+                let res: any = await fetch("/api/post/image?file=" + fileName);
+                res = await res.json();
+                console.log("res", res);
+
+                const formData = new FormData(); // form 태그랑 똑같이 동작함
                 Object.entries({ ...res.fields, file }).forEach(([key, value]) => {
-                  console.log("key, value: ", key, value);
                   formData.append(key, value as string | File)
-                })
+                });
 
                 const uploadRes = await fetch(res.url, {
                   method: "POST",
                   body: formData,
                 })
-                console.log("응답:",uploadRes)
+
+                setImageUrl(uploadRes.url + "/" + fileName);
               }
             }}
           />
-          <img src="" />
+          <img src={imageUrl} />
           <div className="below-btn">
             <button type="submit" className="register-btn">등록</button>
             <button><Link href="/list" className="cancel-btn">취소</Link></button>
